c2b5331724063c280a5186bfda383b5c368ce119
omarzidan97 <omaromraan@gmail.com>
2017-03-08 16:11:50 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,30 +1,46 @@
 <?php
 
-file_put_contents("fb.txt",file_get_contents("php://input"));
+//file_put_contents("fb.txt",file_get_contents("php://input"));
 
-$fb = file_get_contents("fb.txt");
+$input = json_decode(file_get_contents('php://input'), true);
 
-$fb = json_decode($fb);
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
 
-$rid = $fb->entry[0]->messaging[0]->sender->id;
+
+//API Url
 
 $token = "EAAU7xvq2auMBAOl8jo3b4SoNYmlFB7aaXngktoiCvRQs6Tw3bp8an0V03TKfPQjbWJINeiyULavylzi7yZAzQXQW3v9b6CqHuSMY3YurBuZBhWLNJ7dMLZAdSWCblhkPMrtVS7O5nrVZB6FloOR16xiRMwEOdCFI5R5xVg1x4wZDZD"
 
-$data = array(
-	'recipient' => array('id'=> "$rid"),
-	'message' => array('text' => "Hello w ra7mato allah!.. fadaaal")
-);
+$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=$token';
 
-$options = array(
-	'http' => array(
-		'method' => 'POST',
-		'content' => json_encode($data),
-		'header' => "Content-Type: application/json\n"
-	)
-);
+//Initiate cURL.
+$ch = curl_init($url);
 
-$context = stream_context_create($options);
+//The JSON data.
+$jsonData = '{
+    "recipient":{
+        "id":"'.$sender.'"
+    }, 
+    "message":{
+        "text":"Hello w ra7mato allah!... fadaaal"
+    }
+}';
 
-file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token",fales,$context);
+//Encode the array into JSON.
+$jsonDataEncoded = $jsonData;
 
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+//Execute the request
+if(!empty($input['entry'][0]['messaging'][0]['message'])){
+$result = curl_exec($ch);
+}
 ?>
