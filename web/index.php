990ddab7e9bf9b9189cae12af0f90dcc01a76367
omarzidan97 <omaromraan@gmail.com>
2017-03-08 16:15:57 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,46 +1,40 @@
 <?php
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
+// Set this Verify Token Value on your Facebook App 
+if ($verify_token === 'scenesupplybot') {
+  echo $challenge;
+}
 
-file_put_contents("fb.txt",file_get_contents("php://input"));
-
-$input = json_decode(file_get_contents("fb.txt"), true);
-
+$input = json_decode(file_get_contents('php://input'), true);
+// Get the Senders Graph ID
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+// Get the returned message
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
-
-
-//API Url
+//API Url and Access Token, generate this token value on your Facebook App Page
 
 $token = "EAAU7xvq2auMBAOl8jo3b4SoNYmlFB7aaXngktoiCvRQs6Tw3bp8an0V03TKfPQjbWJINeiyULavylzi7yZAzQXQW3v9b6CqHuSMY3YurBuZBhWLNJ7dMLZAdSWCblhkPMrtVS7O5nrVZB6FloOR16xiRMwEOdCFI5R5xVg1x4wZDZD"
 
 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=$token';
-
 //Initiate cURL.
 $ch = curl_init($url);
-
 //The JSON data.
 $jsonData = '{
     "recipient":{
-        "id":"'.$sender.'"
+        "id":"' . $sender . '"
     }, 
     "message":{
-        "text":"Hello w ra7mato allah!... fadaaal"
+        "text":"The message you want to return"
     }
 }';
-
-//Encode the array into JSON.
-$jsonDataEncoded = $jsonData;
-
 //Tell cURL that we want to send a POST request.
 curl_setopt($ch, CURLOPT_POST, 1);
-
 //Attach our encoded JSON string to the POST fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
 //Set the content type to application/json
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-
-//Execute the request
+//Execute the request but first check if the message is not empty.
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
-$result = curl_exec($ch);
+  $result = curl_exec($ch);
 }
 ?>
